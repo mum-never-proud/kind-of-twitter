03e210dd654e217f0b92aded09ff3d9e8da7b2d5
mum-never-proud <abhuz@hotmail.com>
2020-10-06 09:45:42 +0530

    fix validation

--- src/constants/validator-options.js
+++ src/constants/validator-options.js
@@ -7,7 +7,7 @@ export const signUp = {
     [(email) => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email), 'Enter a valid email address.'],
   ],
   password: [
-    [(password) => password && password.length > 8, 'Password must be at least 8 characters.'],
+    [(password) => password && password.length >= 8, 'Password must be at least 8 characters.'],
   ],
 };
 
